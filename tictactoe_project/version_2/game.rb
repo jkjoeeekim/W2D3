04d3cf10aa52d61d9969c40ea0bfd6dd0934c59d
Joseph Kim <jkjoeeekim@live.com>
2021-05-18 15:14:14 -0700

    Finish tictactoe_project/version2

---
+++ tictactoe_project/version_2/game.rb
@@ -0,0 +1,86 @@
+require_relative "./board.rb"
+require_relative "./human_player.rb"
+
+class Game
+    attr_reader :players, :current_player, :board
+
+    def initialize(size, *players_marks)
+        @players = Hash.new
+
+        players_marks.each_with_index do |mark, idx|
+            mark = HumanPlayer.new(size, mark)
+            @players[mark] = "Player #{idx + 1}"
+        end
+        
+        @current_player = @players.keys.first
+        @board = Board.new(size)
+        
+    end
+
+    def player_name
+        players[current_player]
+    end
+
+    def switch_turn
+        @players.keys.each_with_index do |player, idx|
+            if current_player.mark == player.mark
+                @current_player = @players.keys[(idx + 1) % players.keys.length]
+                return true
+            end
+        end
+    end
+
+    def play
+        while board.empty_positions? do
+            puts "\n#{" " * 3}Make a Move #{player_name}\n#{" " * 6}Your Sign is #{current_player.mark}"
+            board.print
+
+            begin
+                position = current_player.get_position
+                @board.place_mark(position, current_player.mark)
+            rescue => error
+                gets
+            end
+
+            if board.win?(current_player.mark)
+                puts "\n#{" " * 8}GAME OVER!"
+                board.print
+                puts "\n#{" " * 5}#{player_name} VICTORY!\n "
+                return
+            elsif board.made_move
+                switch_turn
+                board.made_move = false
+            end
+        end
+        puts "\n#{" " * 8}GAME OVER!"
+        board.print
+        puts "  Tie Game\n "
+    end
+end
+
+new_game = Game.new(5, :X, :O, :K, :J)
+new_game.play
+# p new_game.current_player.mark
+# p new_game.player_name
+# p new_game.switch_turn
+
+# p new_game.current_player.mark
+# p new_game.player_name
+# p new_game.switch_turn
+
+# p new_game.current_player.mark
+# p new_game.player_name
+# p new_game.switch_turn
+
+# p new_game.current_player.mark
+# p new_game.player_name
+# p new_game.switch_turn
+
+# p new_game.current_player.mark
+# p new_game.player_name
+# p new_game.switch_turn
+
+# p new_game.current_player.mark
+# p new_game.player_name
+
+# p new_game.players.keys[1].mark
---
+++ tictactoe_project/version_2/human_player.rb
@@ -0,0 +1,28 @@
+require_relative "./board.rb"
+
+class HumanPlayer
+    attr_reader :mark
+
+    def initialize(size, mark_value)
+        @mark = mark_value
+        @board = Board.new(size)
+    end
+
+    def get_position
+        begin
+            puts "Enter a row between 1 and #{@board.board.length}."
+            row = gets.chomp.to_i
+            if !(1..@board.board.length).to_a.include?(row)
+                raise StandardError.new("\nInvalid row, does not exist.\nPress Enter to try again\n ")
+            end
+            puts "\nEnter a column between 1 and #{@board.board.length}."
+            col = gets.chomp.to_i
+            if !(1..@board.board.length).to_a.include?(col)
+                raise StandardError.new("\nInvalid column, does not exist.\nPress Enter to try again\n ")
+            end
+            [row - 1, col - 1]
+        rescue => error
+            puts error.message
+        end
+    end
+end
